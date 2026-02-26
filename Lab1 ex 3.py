b46ad9f53cfe5789667d402c4f19e87823217911
Gustavo-noronha <10769664@mackenzista.com.br>
2026-02-26 11:56:22 -0300

    Delete Lab1 ex 3.py

--- Lab1 ex 3.py
+++
@@ -1,5 +0,0 @@
-V=float(input('qual a  velocidade inicial do trem em M/S ?'))
-T=float(input('qual foi o tempo de frenagem em segundos?'))
-A=-V/T
-S=V*T+A*T**2/2
-print('a distancia que a trem percorreu ate parar foi de :',S,'metros')
