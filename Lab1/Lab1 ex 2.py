e772cbed1bcbc6f60148fccb4d5232a25b483d6d
Gustavo-noronha <10769664@mackenzista.com.br>
2026-02-26 11:55:13 -0300

    Add files via upload

---
+++ Lab1/Lab1 ex 2.py
@@ -0,0 +1,4 @@
+M=float(input('qual massa da bola de boliche em KG:'))
+V=float(input('qual a velocidade da bola de boliche em M/S:'))
+Ec=M*V**2/2
+print('a energia cinética de impacto da bola de boliche `e :',Ec,'J')
