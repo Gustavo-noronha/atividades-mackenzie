f9ab0506a157291a2e0f351e65dcfa460091cc7f
Gustavo-noronha <10769664@mackenzista.com.br>
2026-02-26 11:58:15 -0300

    Delete lab2 ex 2.py

--- lab2 ex 2.py
+++
@@ -1,7 +0,0 @@
-videos=float(input('quantos vídeos?'))
-InsG=videos//100
-restoG=videos%100
-InsM=restoG//10
-InsP=restoG%10
-total=InsG*150+InsM*20+InsP*5
-print(InsG,'Instancias G',InsM,'Instâncias M',InsP,'Instâncias P. Custo:',total )
