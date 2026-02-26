f0e4f95f0bc5be752fd5044e0f2d3dd95e28ddbe
Gustavo-noronha <10769664@mackenzista.com.br>
2026-02-26 11:55:39 -0300

    Delete Lab1 ex 1.py

--- Lab1 ex 1.py
+++
@@ -1,4 +0,0 @@
-M=float(input('qual a massa do carro'))
-A=float(input('qual a taxa de desaceleração(em modulo)'))
-F=M*A
-print('A força necessária para desacelerar um carro de massa ',M,'com uma taxa de desaceleração',A,'`e de:',F,'N')
