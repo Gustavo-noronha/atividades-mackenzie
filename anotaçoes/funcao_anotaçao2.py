f0674dab94c83252825833759ba9b75d1cd737f6
Gustavo-noronha <10769664@mackenzista.com.br>
2026-03-11 20:07:04 -0300

    Update funcao_anotaçao2.py

--- anotaçoes/funcao_anotaçao2.py
+++ anotaçoes/funcao_anotaçao2.py
@@ -1,4 +1,4 @@
-def soma (x,y):# cria a funçao chamda soma
+def soma (x,y):# cria a funçao chamada soma
     return x+y # retorna a soma dos numeros
 n1=int(input('digite um numero'))
 n2=int(input('digite outro numero'))
